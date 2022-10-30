65cea622e14c390a4b53c04ec7b14cba3022b7dc
Konstantin Sosnovskiy <coolhit74@mail.ru>
2022-10-31 00:22:24 +0300

    Age filter added

--- src/PersonNameLengthComparator.java
+++ src/PersonNameLengthComparator.java
@@ -1,20 +1,19 @@
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PersonNameLengthComparator implements Comparator<Person> {
     private int maxWordsOofSurname;
+    private Predicate<Person> less18;
 
     public PersonNameLengthComparator(int maxWordsOfSurname) {
         this.maxWordsOofSurname = maxWordsOfSurname;
+        this.less18 = age -> age.getAge() < 18;
     }
 
     @Override
     public int compare(Person o1, Person o2) {
-//        if (o1.getSurName().contains("-")) {
         int qtyWords1 = o1.getSurName().split("-").length;
-
-//        if (o2.getSurName().contains("-")) {
         int qtyWords2 = o2.getSurName().split("-").length;
-
         if (qtyWords1 == qtyWords2 || qtyWords1 > maxWordsOofSurname || qtyWords2 > maxWordsOofSurname) {
             return Integer.compare(o2.getAge(), o1.getAge());
         } else {
